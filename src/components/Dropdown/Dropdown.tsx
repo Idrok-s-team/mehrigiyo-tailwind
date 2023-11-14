559aba0826f0c465195c61690299518a32db95e4
@riskiddin.rustamov <riskiddin.rustamov@paynet.uz>
2023-11-14 10:23:03 +0500

    configured tailwind and next

---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,84 @@
+import { type FC, useState, useRef, memo, useEffect, ReactNode } from "react";
+import { useOnClickOutside } from "usehooks-ts";
+
+export interface ItemProps {
+  id: string;
+  label: ReactNode;
+  selected?: boolean;
+}
+
+interface IProps {
+  items: ItemProps[];
+  className?: string;
+  // onClick: (item: ItemProps) => void;
+}
+
+export const Dropdown: FC<IProps> = memo(function Dropdown({
+  items,
+}: // onClick,
+IProps) {
+  const [open, setOpen] = useState<boolean>(false);
+  const [selectedItem, setSelectedItem] = useState<ItemProps | null>(null);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const selected = items.find((item) => item.selected) || items[0];
+    setSelectedItem(selected);
+  }, [items]);
+
+  useOnClickOutside(ref, () => {
+    setOpen(false);
+  });
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleSelectItem = (item: ItemProps) => {
+    setSelectedItem(item);
+    setOpen(false);
+  };
+
+  return (
+    <div className="relative w-full h-full" ref={ref}>
+      <button
+        id="dropdownHoverButton"
+        data-dropdown-toggle="dropdownHover"
+        data-dropdown-trigger="hover"
+        className="flex items-center justify-between  h-full px-2 text-center rounded-lg focus:outline-none"
+        type="button"
+        onClick={handleToggle}
+      >
+        <span className="mr-2">{selectedItem?.label}</span>
+        <svg
+          className="w-2.5 h-2.5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 10 6"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="m1 1 4 4 4-4"
+          />
+        </svg>
+      </button>
+      {open && (
+        <div className="flex flex-col absolute min-w-full z-10 bg-white rounded-lg shadow">
+          {items.map((item) => (
+            <button
+              key={item.id}
+              onClick={() => handleSelectItem(item)}
+              className="py-1 hover:bg-green-light"
+            >
+              {item.label}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,25 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "green-dark": "#458E5F",
+        "green-primary": "#53b175",
+        "green-light": "#E5FCED",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
